2bfcaf0ea1c4ae8073647a33cac49cab8364e865
Rydkin Sergey <s.rydkin@npcmr.ru>
2022-06-01 22:04:43 +0300

    fix auth url

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -6,9 +6,6 @@ from .views import UserViewSet, CommentViewSet, ReviewViewSet, TitleViewSet, get
 app_name = 'api'
 
 router = routers.DefaultRouter()
-router.register(
-    '', UserViewSet
-)
 router.register('users', UserViewSet)
 router.register('titles', TitleViewSet)
 router.register(
@@ -25,7 +22,7 @@ router.register(
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/signup/', send_code, name='send_code'),
-    path('v1/token/', get_token, name='get_token')
+    path('v1/auth/signup/', send_code, name='send_code'),
+    path('v1/auth/token/', get_token, name='get_token')
     # path('v1/', include('djoser.urls.jwt')),
 ]
